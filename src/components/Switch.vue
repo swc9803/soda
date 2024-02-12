85a52c7c2c7887d1ea1383966ebca6371f8358ee
swc9803 <swc9803@gmail.com>
2024-02-12 17:54:01 +0900

    switch 컴포넌트 추가

---
+++ src/components/Switch.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="switch_container">
+    <div class="switch_wrapper">
+      <button v-for="item in flavor" :key="item.id">{{ item.name }}</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// lang="ts"
+const flavor = [
+  { name: "Grape", color: "#6F2DA8" },
+  { name: "Peach", color: "#FFE5B4" },
+  { name: "Orange", color: "#FFA500" },
+  { name: "Strawberry", color: "#FC5A8D" },
+  { name: "Lime", color: "#BFFF00" },
+];
+</script>
+
+<style lang="scss" scoped>
+.switch_container {
+  width: 100%;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <Model />
+    <Switch />
   </div>
 </template>
 
 <script setup>
 import Model from "@/components/Model.vue";
+import Switch from "@/components/Switch.vue";
 </script>
 
 <style lang="scss" scoped>
