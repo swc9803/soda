052465b352ca885eeebac9703c0a462a12dd0143
swc9803 <swc9803@gmail.com>
2024-02-14 23:49:17 +0900

    이름 변경 및 배경 색 추가

--- src/components/Interface.vue
+++ src/components/Interface.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="switch_container">
+    <div class="background" :style="{ backgroundColor: currentColor }" />
+    <div class="name">{{ currentName }}</div>
     <div class="switch_wrapper">
       <button
         v-for="item in flavor"
         :key="item.id"
-        @click="selectColor(item.color)"
+        @click="selectFlavor(item.color, item.name)"
       >
         {{ item.name }}
       </button>
@@ -24,12 +26,14 @@ const flavor = [
 ];
 
 const currentColor = ref("#4EBC38");
+const currentName = ref("Grape");
 const preventClick = ref(false);
 const emit = defineEmits(["changeColor"]);
-const selectColor = (color) => {
+const selectFlavor = (color, name) => {
   if (!preventClick.value && currentColor.value !== color) {
     preventClick.value = true;
     currentColor.value = color;
+    currentName.value = name;
     emit("changeColor", color);
     setTimeout(() => {
       preventClick.value = false;
@@ -41,5 +45,31 @@ const selectColor = (color) => {
 <style lang="scss" scoped>
 .switch_container {
   width: 100%;
+  height: 100vh;
+  .background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+  .name {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    color: white;
+    font-size: 4em;
+    font-weight: 600;
+    z-index: 1;
+  }
+  .switch_wrapper {
+    position: absolute;
+    bottom: 10%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    display: flex;
+    justify-content: space-between;
+    width: 60%;
+  }
 }
 </style>
